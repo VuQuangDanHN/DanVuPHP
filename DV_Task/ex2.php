e3a4fd0aeaa307e9a334c32d138b89341c2576d2
Quang Dan Vu <126580674+VuQuangDanHN@users.noreply.github.com>
2024-01-29 20:58:35 +0200

    Update ex2.php

--- DV_Task/ex2.php
+++ DV_Task/ex2.php
@@ -1,61 +1,78 @@
-<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<section>
-<body>
-<h3>3.1 Write PHP code to output the following message: Hello world! My name is "David"
-</h3>
-<?php
-    // Use backslashes to escape double quotes or use single quotes
-    echo 'Hello world! My name is "Dan Vu"';
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T13 Contact Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="styles-contact.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles-header-footer.css">
+    <!-- Add your favicon here -->
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="styles-header-footer.css" rel="stylesheet">
+</head>
 
-<h3>3.2 Create a PHP variable named $title and assign it the value "PHP is interesting." Then, use this variable as the content within an (heading 4) element.</h3>
-<?php
-    $title = "PHP is interesting.";
-    echo "<h4>$title</h4>";
-    ?>
-<h3>
-3.3 Define three variables: $g1 = 5, $g2 = 4, and $g3 = 5. These variables represent the grades of three students in the course. To display this information, create an HTML table within your PHP code. The table should be structured with columns for Serial Number (S.n.), Name, and Grade, and it should look like this:
-</h3>
-<?php
-    $g1 = 5;
-    $g2 = 4;
-    $g3 = 5;
-    
-?>
+<body class="d-flex-column">
+  <!-- Header -->
+  <div class="header-container align-content-center fs-5 fw-bold text-dark">
+    <!-- Logo -->
+    <div class="logo-container alignment">
+      <img src="./img/logo.png" alt="logo">
+    </div>
+    <!-- Navigation -->
+    <div class="nav-container">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse nav-item"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php"
+                  >Dan Vu</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ex1.php">Ex1</a>
+              </li><li class="nav-item">
+                <a class="nav-link" href="ex2.php">Ex2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ex3.php">Ex3</a>
+              </li><li class="nav-item">
+                <a class="nav-link" href="ex4.php">Ex4</a>
+              </li>
+            </ul>
+            <div class="search">
+              <input
+                type="text"
+                class="search-input"
+                placeholder="Search"
+                name="Search">
+              <a href="#" class="search-icon">
+                <img src="img/icons8-search.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
 
-<table>
-    <tr>
-        <th>S.n.</th>
-        <th>Name</th>
-        <th>Grade</th>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td>John</td>
-        <td><?php echo $g1 ?></td>
-    
-    </tr>
-    <tr>
-        <td>2</td>
-        <td>Alice</td>
-        <td><?php echo $g2 ?></td>
-    
-    </tr>
-    <tr>
-        <td>3</td>
-        <td>Bob</td>
-        <td><?php echo $g3 ?></td>
-    
-    </tr>
-
-</table>
-<h3>3.4 Take a screenshot that confirms your development environment setup and include it as an image in the "ex1.php" file.</h3>
-
-<img src="./img/docker.png" alt="web development environment"/>
-
-
-</body>
-</section>
-
-<?php include 'footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
